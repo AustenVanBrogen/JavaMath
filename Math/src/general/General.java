558a8c778a73bb1fffe4ef5ea34da29fb84d8b57
Austen Van Brogen <43625899+AustenVanBrogen@users.noreply.github.com>
2020-05-18 22:40:56 -0500

    Added a static summation function and a PassingFunction interface so the user can create their own functions to pass into the summation function

--- Math/src/general/General.java
+++ Math/src/general/General.java
@@ -2,4 +2,17 @@ package general;
 
 public class General {
 
+	
+	public static double summation(int nStart, int nEnd, PassingFunction aFunction)
+	{
+		if(aFunction == null)
+			throw new IllegalArgumentException();
+		
+		double result = 0.0;
+		for(int i = nStart; i <= nEnd; i++)
+		{
+			result += aFunction.someFunction(i);
+		}
+		return result;
+	}
 }
---
+++ Math/src/general/PassingFunction.java
@@ -0,0 +1,5 @@
+package general;
+
+public interface PassingFunction {
+	double someFunction(double xValue);
+}
